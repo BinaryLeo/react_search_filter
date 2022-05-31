c4c793c88d96e217b3fcc674205011bae867737a
binaryleo <leomouracss@fastmail.com>
2022-05-31 08:58:48 -0300

    chore: filter options

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,59 @@
 import "./app.css"
-import {Users} from "./users"
-import {useState} from 'react';
+import { Users } from "./users"
+import { useState } from 'react';
 import { Table } from "./Table";
-function App(){
+
+//*BASIC SEARCH
+/* function App() {
   const [query, setQuery] = useState("");
-  //query gets the value of the input field amd setQuery sets the value each time the input field changes
-  const searchUsers = (data) => {
-  return data.filter(item => 
-   item.first_name.toLowerCase().includes(query)||
-   item.last_name.toLowerCase().includes(query)||
-   item.email.toLowerCase().includes(query))
-  }
-
+  //*query gets the value of the input field and
+  //*setQuery sets the value each time the input field changes
   return (
-
     <div className="app">
-    <input type="text" className="search" placeholder="Search" onChange={e=>setQuery(e.target.value)}></input>
-    
-    <Table data ={searchUsers(Users)}/>
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+      <ul className="list">
+        {
+          Users.filter(user => user.first_name.toLowerCase().includes(query))
+          .map((user) => (
+            <li key={user.id} className="list-item">{user.first_name}</li>
+          ))
+        }
+      </ul>
     </div>
+  );
+} */
 
-  )
+//*Using a Data Table
+function App() {
+  const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email"];
+  const searchUsers = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
 
+  /* const searchUsers = (data) => {
+    return data.filter(item => 
+     item.first_name.toLowerCase().includes(query)||
+     item.last_name.toLowerCase().includes(query)||
+     item.email.toLowerCase().includes(query))
+    } */
+    
+  return (
+    <div className="app">
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+      {<Table data={searchUsers(Users)} />}
+    </div>
+  );
 }
+
 export default App;
