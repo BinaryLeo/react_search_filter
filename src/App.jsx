fad0968c586271c65bb72d331b2511a92a47a7d1
binaryleo <leomouracss@fastmail.com>
2022-05-31 07:23:21 -0300

    chore: table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import "./app.css"
 import {Users} from "./users"
 import {useState} from 'react';
+import { Table } from "./Table";
 function App(){
   const [query, setQuery] = useState("");
+  //query gets the value of the input field amd setQuery sets the value each time the input field changes
+  const searchUsers = (data) => {
+  return data.filter(item => 
+   item.first_name.toLowerCase().includes(query)||
+   item.last_name.toLowerCase().includes(query)||
+   item.email.toLowerCase().includes(query))
+  }
+
   return (
 
     <div className="app">
     <input type="text" className="search" placeholder="Search" onChange={e=>setQuery(e.target.value)}></input>
-    <ul className="list">
-    {
-      Users.filter(user=>user.first_name.toLowerCase().includes(query)).map((user)=>(
-        <li key={user.id} className="list-item">{user.first_name}</li>
-      ))
-    }
-    </ul>
+    
+    <Table data ={searchUsers(Users)}/>
     </div>
 
   )
