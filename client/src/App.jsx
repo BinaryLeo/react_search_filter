7cdbc6e739a3e5aa543076cefddda850bf794fc1
binaryleo <leomouracss@fastmail.com>
2022-05-31 23:21:16 -0300

    chore : add axios dep.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./app.css"
 import { Users } from "./users"
-import { useState } from 'react';
+import { useState, useEffect } from  'react';
 import { Table } from "./Table";
-
+import axios from "axios";
+//---------------------------------------------------------
 //*BASIC SEARCH
+//---------------------------------------------------------
 /* function App() {
   const [query, setQuery] = useState("");
   //*query gets the value of the input field and
@@ -26,16 +28,17 @@ import { Table } from "./Table";
     </div>
   );
 } */
-
+//---------------------------------------------------------
 //*Using a Data Table
-function App() {
+//---------------------------------------------------------
+/* function App() {
   const [query, setQuery] = useState("");
   const keys = ["first_name", "last_name", "email"];
   const searchUsers = (data) => {
     return data.filter((item) =>
       keys.some((key) => item[key].toLowerCase().includes(query))
     );
-  };
+  }; */
 
   /* const searchUsers = (data) => {
     return data.filter(item => 
@@ -43,7 +46,21 @@ function App() {
      item.last_name.toLowerCase().includes(query)||
      item.email.toLowerCase().includes(query))
     } */
-    
+//---------------------------------------------------------
+
+//** */ API SEARCH
+function App(){
+
+  const [query, setQuery] = useState("");
+  const[data, setData] = useState([])
+  useEffect(() => {
+   const fetchUsers = async () => {
+     const res =  await axios.get('http://localhost:5000');
+      setData(res.data);
+   };
+   fetchUsers();
+
+  }, []);
   return (
     <div className="app">
       <input
@@ -51,7 +68,7 @@ function App() {
         placeholder="Search..."
         onChange={(e) => setQuery(e.target.value.toLowerCase())}
       />
-      {<Table data={searchUsers(Users)} />}
+      {<Table data={data} />}
     </div>
   );
 }
